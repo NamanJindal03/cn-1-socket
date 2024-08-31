c17a7f2ac243ffe355b7cf19337ab5c33bf91f81
Naman Jindal <namanjindal113@gmail.com>
2024-08-31 21:19:37 +0530

    socket communication more events added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 
+let users = 0;
+
 const server = http.createServer(app);
 
 const io = new Server(server, {
@@ -19,13 +21,27 @@ io.on('connection', (socket) => {
     console.log('Connection intiated');
 
     //handle socket code
-    socket.on('first-timer', (message) => {
-        console.log(message);
-        socket.emit('revert', 'yes yes yes yes');
+    // socket.on('first-timer', (message) => {
+    //     console.log(message);
+    //     socket.emit('revert', 'yes yes yes yes');
+    // })
+    socket.on('store-user-info', (username)=>{
+        socket.userInfo = username;
+    })
+
+    //whenever I have to update the user count, I will be initiating this event
+    socket.emit('users', users+1);
+
+    socket.emit('weather_info_update', 'some new weather info')
+
+    socket.on('is_typing', (data)=>{
+        console.log(socket.userInfo)
+        console.log('user is busy doing', data)
     })
 
     socket.on('disconnect', ()=>{
-        console.log('connection disconnected')
+        console.log('connection disconnected');
+        users = users -1;
     })
 })
 
